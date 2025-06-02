b0baede99f89f216ef9516cd5b230a67efadf755
satwika <chitrasatwika@gmail.com>
2025-06-02 11:38:59 +0530

    merge intervals

--- Greedy/56. Merge Intervals.py
+++ Greedy/56. Merge Intervals.py
@@ -1,52 +1,114 @@
-# Note: Here one interval must start after end of another interval.
+# Intuition first, code next ( Python, Java and C++ ) and time complexity analysis at the end.
 
-# logic: if start time of next intervals is <= end time of pre interval means they are overlapping i.e
-#  current ongoing meeting has not ended till and next one has started=> intervals are overlapping so merge them.
+# INTUITION : 
 
-# 1st sort a/c to starting time and 
-# Agar overlap kar rha then start time to pichle wale ka hi rhega but end me 'dono ka maximum ending time' ho jayega.
+'''
+We need to merge the intervals into the minimum number of intervals such that no two intervals overlap.
+To do this, we can sort the intervals based on their start times and then iterate through them, merging overlapping intervals as we go.
+
+We will add the non-overlapped intervals into an result arry , which we are going to return at the last. 
+
+Initially, the first array will be added to the result array.
+
+Then onwards, we will compare the endTime of the last interval in the result array with the startTime of the current interval.
+If the startTime of the current interval is greater than the endTime of the last interval in the result array, it means the intervals are non-overlapping, and we can add the current interval to the result array.
+
+Also, if the startime is equal to, or less than the endtime of the previous, then it means they are overlapping, which means we cant add the current interval into the result.
+
+But, here's a catch, which many might miss - don't forget to update the endtime of the previous intervals with the maximum of current endtTime and previous endTime.
+
+After the iteration, we will return the result array which will contain the merged intervals.
+'''
+
+# CODE : 
+
+# PYTHON : 
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()  
-        # intervals.sort(key= lambda i: i[0])   # 'i': intervals, i[0] index value on which we want to sort
-        output= [intervals[0]]  # to handle the edge case and make comparison easy
-        for start, end in intervals[1:]:
-            # check if ending of last added interval is >= than starting of the currnet one 
-            if output[-1][1] >= start: 
-		# i.e if overlapping then merge , make end of last added one max(end of last addded, end)
-                output[-1][1]= max(output[-1][1], end)    # [[1,4],[2,3]]= [[1,4]]
-            else:
-		# If not overlapping then add directly 
-                output.append([start, end])  # just the cur interval only.
-        return output
+        n = len(intervals)
+        intervals.sort()
+        prev = -1 
+        res = []
+        for i in range(n):
+            if intervals[i][0] > prev :
+                res.append([intervals[i][0],intervals[i][1]])
+                prev = intervals[i][1]
+            elif res :
+                res[-1][1] = max(res[-1][1],intervals[i][1])
+                prev = res[-1][1]
+        return res
 
-# Java code.
+        
 
-# other way of java code. correct one
-"""
+# JAVA : 
+
+
+'''
 class Solution {
-	public int[][] merge(int[][] intervals) {
-		if (intervals.length <= 1)
-			return intervals;
-
-		// Sort by ascending starting point
-		Arrays.sort(intervals, (i1, i2) -> i1[0], i2[0]);
-
-		List<int[]> result = new ArrayList<>();
-		int[] newInterval = intervals[0];
-		result.add(newInterval);
-		for (int[] interval : intervals) {
-			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
-				newInterval[1] = Math.max(newInterval[1], interval[1]);
-			else {                             // Disjoint intervals, add the new interval to the list
-				newInterval = interval;
-				result.add(newInterval);
-			}
-		}
-
-		return result.toArray(new int[result.size()][]);
-	}
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> res = new ArrayList<>();
+        int prev = -1;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] > prev) {
+                res.add(new int[]{intervals[i][0], intervals[i][1]});
+                prev = intervals[i][1];
+            } else {
+                int[] last = res.get(res.size() - 1);
+                last[1] = Math.max(last[1], intervals[i][1]);
+                prev = last[1];
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
 }
 
-"""
+'''
+
+
+
+# C++ : 
+
+
+
+'''
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        int prev = -1;
+        for (int i = 0; i < intervals.size(); i++) {
+            if (intervals[i][0] > prev) {
+                res.push_back({intervals[i][0], intervals[i][1]});
+                prev = intervals[i][1];
+            } else {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+                prev = res.back()[1];
+            }
+        }
+        return res;
+    }
+};
+
+'''
+
+
+
+
+# TIME COMPLEXITY :
+
+'''
+-> Sorting the array = 0(nlogn)
+-> Iterating through the array = O(n)
+-> Overall time complexity = O(nlogn)
+'''
+
+# SPACE COMPLEXITY :
+
+'''
+-> O(n) for the result array.
+-> O(1) for the variables used.
+-> Overall space complexity = O(n)
+'''
